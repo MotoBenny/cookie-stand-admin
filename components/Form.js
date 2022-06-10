a09281a69afa908decd663ae18953b2ca254f3d9
Benjamin <benj.james.carter@gmail.com>
2022-06-09 18:44:34 -0700

    WIP

--- components/Form.js
+++ components/Form.js
@@ -1,5 +1,9 @@
+import { hours } from '../pages/data'
+
 export default function Form(props) {
+  console.log(props)
   return (
+    <>
     <form className='grid grid-rows-2 w-1/2 p-2 bg-emerald-300 rounded-md my-10' onSubmit={props.onSubmit}>
       <h1 className='flex justify-center py-3 text-2xl'>Create Cookie Stand</h1>
       <div className="grid col-span-2">
@@ -20,5 +24,97 @@ export default function Form(props) {
         </div>
       </div>
     </form>
+    <ReportTable locJson={props}/>
+    </>
   )
+  
+  function ReportTable(props) {
+    function calcHourlyTotal() {
+      let globalTotal = 0;
+      let hourlyTotal = []
+      for (let i = 0; i < hours.length; i++) {
+        let sumHour = 0;
+        locJson.map(elem => {
+          sumHour += elem.HourlySales[i]
+        })
+        hourlyTotal[i] = sumHour
+      }
+      globalTotal = hourlyTotal.reduce((acc, curr) => acc + curr)
+      hourlyTotal[hours.length] = globalTotal;
+      return (hourlyTotal)
+    }
+    // if no results print "results coming soon"
+    // if results render table
+    function drawReportTable() {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <td>
+                Location
+              </td>
+              {props.locJson.map(hour => {
+                return (
+                  <td key={hour}>
+                    {hour}
+                  </td>
+                )
+              })}
+              <td>
+                Totals
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {props.location.map(location => {
+              return (
+                <tr key={location.Location}>
+                  <td>{location.Location}</td>
+                  {location.HourlySales.map(hourSales => {
+                    return (
+                      <td className='border-2 border-black' key={hourSales}>
+                        {hourSales}
+                      </td>
+                    );
+                  })
+                  }
+                  <td>
+                    {
+                      location.HourlySales.reduce((acc, curr) => acc + curr)
+                    }
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tfoot>
+            <tr className='border-2 border-black'>
+              <td className='border-2 border-black'>Totals</td>
+              {calcHourlyTotal().map(elem => {
+                return (
+                  <td className='border-2 border-black' key={elem}>{elem}</td>
+                )
+              })}
+            </tr>
+          </tfoot>
+        </table>
+      )
+    }
+  
+    function reportTable(props) {
+      console.log(props)
+      if (props.locJson.length === 0) {
+        return (
+          <p>No stand data</p>
+        )
+      } else {
+        return drawReportTable()
+      }
+    }
+    // return (
+    //   reportTable(props)
+    // )
+  }
+  
+
 }
